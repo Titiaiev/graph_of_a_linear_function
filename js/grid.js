21b562738bf07cf96c7615745b1aea0aa3f96afa
Maksim Titiaiev <gipsmas@gmail.com>
2017-10-06 23:33:26 +0300

    отформатировал код

--- js/grid.js
+++ js/grid.js
@@ -1,39 +1,38 @@
 drawGrid(25);
 
-function drawGrid(step){
+function drawGrid(step) {
+    "use strict"
+    var canvas2 = document.getElementById("grid");
+    var grid = canvas2.getContext("2d");
 
-"use strict"
-var canvas2 = document.getElementById("grid");
-var grid = canvas2.getContext("2d");
+    var gridWidth = canvas2.width,
+        gridHeight = canvas2.height;
 
-var gridWidth = canvas2.width,
-    gridHeight = canvas2.height;
+    var step = step || 25;
 
-var step = step || 25;
+    grid.strokeStyle = "black";
+    grid.lineWidth = 1;
 
-grid.strokeStyle = "black";
-grid.lineWidth = 1;
+    grid.beginPath();
+    for (var i = gridWidth / 2 + step; i < gridWidth - step; i += step) {
+        grid.moveTo(0, i);
+        grid.lineTo(gridWidth, i);
+    }
 
-grid.beginPath();
-for (var i = gridWidth / 2 + step; i < gridWidth - step; i += step) {
-    grid.moveTo(0, i);
-    grid.lineTo(gridWidth, i);
-}
+    for (var i = gridWidth / 2 - step; i > step; i -= step) {
+        grid.moveTo(0, i);
+        grid.lineTo(gridWidth, i);
+    }
 
-for (var i = gridWidth / 2 -step; i > step; i -= step) {
-    grid.moveTo(0, i);
-    grid.lineTo(gridWidth, i);
-}
+    for (var i = gridHeight / 2 + step; i < gridHeight - step; i += step) {
+        grid.moveTo(i, 0);
+        grid.lineTo(i, gridHeight);
+    }
 
-for (var i = gridHeight / 2 + step; i < gridHeight - step; i += step) {
-    grid.moveTo(i, 0);
-    grid.lineTo(i, gridHeight);
-}
-
-for (var i = gridHeight / 2 - step; i > step; i -= step) {
-    grid.moveTo(i, 0);
-    grid.lineTo(i, gridHeight);
-}
-grid.stroke();
+    for (var i = gridHeight / 2 - step; i > step; i -= step) {
+        grid.moveTo(i, 0);
+        grid.lineTo(i, gridHeight);
+    }
+    grid.stroke();
 
 };
